82d9506593412298e210322d00c7878766bab70c
PandaMia <60604574+PandaMia@users.noreply.github.com>
2020-07-03 12:45:00 +0300

    Add files via upload

---
+++ sorting/quick_sort.py
@@ -0,0 +1,27 @@
+from random import randint
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        left = []
+        mid = []
+        right = []
+        pivot = array[len(array)//2]
+        for num in array:
+            if num < pivot:
+                left.append(num)
+            elif num == pivot:
+                mid.append(num)
+            else:
+                right.append(num)
+        return quick_sort(left) + mid + quick_sort(right)
+
+
+array = [randint(0, 50) for i in range(10)]
+
+sort_array = quick_sort(array)
+
+print(array)
+print(sort_array)
+
