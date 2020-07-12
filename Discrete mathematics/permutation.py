acf17fb99cb561d5aa9cdb068b61ccf5019e0d87
PandaMia <60604574+PandaMia@users.noreply.github.com>
2020-07-12 12:43:35 +0300

    Add files via upload

---
+++ Discrete mathematics/permutation.py
@@ -0,0 +1,15 @@
+# Нужно сгенерировать все возможные k-перестановки n-элементов без повторений.
+def permutation(n, k, seq = ''):
+
+    if k > n:
+        print()
+    elif len(seq) == k:
+        print(*seq)
+    else:
+        for elem in range(n):
+            if str(elem) not in seq:
+                permutation(n, k, seq = seq + str(elem))
+ 
+n, k = [int(i) for i in input().split()]
+perm = [int(i) for i in range(k)]
+permutation(n, k)
